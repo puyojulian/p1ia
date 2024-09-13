1feec1a770c49d495a1277ce7fc7e74b31a3c20b
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2024-09-13 14:44:53 -0500

    added the file reading function

--- busqueda/src/main.go
+++ busqueda/src/main.go
@@ -1,8 +1,5 @@
 package main
 
-// import {
-// 	"github.com/puyojulian/p1ia/busqueda/src/search"
-// }
-
 func main() {
+
 }
--- busqueda/src/search/utils.go
+++ busqueda/src/search/utils.go
@@ -1,1 +1,46 @@
 package search
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_matrix() ([][]int, error) {
+	// Open the file
+	file, err := os.Open("Prueba1.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a 2D slice to store the matrix
+	var matrix [][]int
+
+	// Read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Split the line by spaces to get individual string numbers
+		stringValues := strings.Fields(line)
+
+		// Convert string numbers to integers and append to a row
+		var row []int
+		for _, stringValue := range stringValues {
+			num, err := strconv.Atoi(stringValue)
+			if err != nil {
+				fmt.Println("Error converting to integer:", err)
+				return nil, err
+			}
+			row = append(row, num)
+		}
+
+		// Append the row to the matrix
+		matrix = append(matrix, row)
+	}
+
+	return matrix, nil
+}
